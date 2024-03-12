1a11ef1b43b94c1364c0385d35ccad48180d4ed3
PremR <prem.rajah@gmail.com>
2024-03-12 13:42:49 +0000

    added custom spinner with react-spinners

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
 
-const LoadingPage = () => {
-  return <div>loading...</div>;
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = ({ loading }) => {
+  return (
+    <ClipLoader color='#3b82f6' loading={loading} cssOverride={override} size={150} aria-label='Loading Spinner' />
+  );
 };
 
 export default LoadingPage;
