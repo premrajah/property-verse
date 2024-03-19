5ddd1ac03068716ad74016363b32db85470a1081
PremR <prem.rajah@gmail.com>
2024-03-19 12:50:45 +0000

    added nextAuth provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,19 @@
 import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
