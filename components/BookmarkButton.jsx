bf46c259b8bc62b91e6960dade009c7b5a6ce399
PremR <prem.rajah@gmail.com>
2024-04-19 23:02:15 +0100

    added bookmark button and share proerty button

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,5 +1,12 @@
+import { FaBookmark } from "react-icons/fa";
+
 const BookmarkButton = ({ property }) => {
-  return <div>BookmarkButton</div>;
+  return (
+    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+      <FaBookmark className='fas fa-bookmark mr-2' />
+      Bookmark Property
+    </button>
+  );
 };
 
 export default BookmarkButton;
--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,5 +1,12 @@
+import { FaShare } from "react-icons/fa";
+
 const ShareButtons = ({ property }) => {
-  return <div>ShareButtons</div>;
+  return (
+    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+      <FaShare className='fas fa-bookmark mr-2' />
+      Share Property
+    </button>
+  );
 };
 
 export default ShareButtons;
