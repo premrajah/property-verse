e876ed39bcd7f6957b7e9a0b0d6e6fbb68642224
PremR <prem.rajah@gmail.com>
2024-04-26 22:27:08 +0100

    added bookmark check route, fixed bookmark route and completed bookmark button funcationality

--- app/api/bookmarks/check/route.js
+++ app/api/bookmarks/check/route.js
@@ -27,24 +27,8 @@ export const POST = async (request) => {
         // check if property is bookmarked
         let isBookmarked = user.bookmarks.includes(propertyId);
 
-        let message;
-
-        if (isBookmarked) {
-            // if already bookmarked remove it
-            user.bookmarks.pull(propertyId);
-            message = "Bookmark removed successfully."
-            isBookmarked = false;
-        } else {
-            // if not bookmarked add it 
-            user.bookmarks.push(propertyId);
-            message = "Bookmark added successfully.";
-            isBookmarked = true;
-        }
-
-        await user.save();
 
         return new Response(JSON.stringify({
-            message,
             isBookmarked
         }), { status: 200 })
 
