64d3e86b83bdea425466dde9dd02d7c3f8bb2475
PremR <prem.rajah@gmail.com>
2024-04-29 11:07:20 +0100

    created property search forma nd setup basic content

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,5 @@
+import PropertySearchForm from "./PropertySearchForm";
+
 const Hero = () => {
   return (
     <section className='bg-blue-700 py-20 mb-4'>
@@ -6,7 +8,7 @@ const Hero = () => {
           <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>Find The Perfect Rental</h1>
           <p className='my-4 text-xl text-white'>Discover the perfect property that suits your needs.</p>
         </div>
-        {/* <PropertySearchForm /> */}
+        <PropertySearchForm />
       </div>
     </section>
   );
