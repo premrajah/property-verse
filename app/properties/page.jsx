88fb76902b249d317a3589379608ee09a05f2263
PremR <prem.rajah@gmail.com>
2024-04-29 12:25:43 +0100

    added search from to properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,6 @@
 import { fetchProperties } from "@/utils/requests";
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
@@ -8,19 +9,27 @@ const PropertiesPage = async () => {
   properties.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <section className='px-4 py-6'>
-      <div className='container-xl lg:container m-auto px-4'>
-        {properties.length === 0 ? (
-          <p>No Properties Found</p>
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3'>
-            {properties.data.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className='bg-blue-700 py-4'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <PropertySearchForm />
+        </div>
+      </section>
+
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4'>
+          {properties.length === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3'>
+              {properties.data.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
