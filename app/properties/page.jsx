8e6188702ed5db2c77934b7d6573e7e235668b97
PremR <prem.rajah@gmail.com>
2024-05-01 12:32:22 +0100

    refactored properties to its own client side component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,13 +1,7 @@
-import { fetchProperties } from "@/utils/requests";
-import PropertyCard from "@/components/PropertyCard";
 import PropertySearchForm from "@/components/PropertySearchForm";
+import Properties from "@/components/Properties";
 
 const PropertiesPage = async () => {
-  const properties = await fetchProperties();
-
-  // sort by date
-  properties.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <section className='bg-blue-700 py-4'>
@@ -16,19 +10,7 @@ const PropertiesPage = async () => {
         </div>
       </section>
 
-      <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto px-4'>
-          {properties.length === 0 ? (
-            <p>No Properties Found</p>
-          ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3'>
-              {properties.data.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
---
+++ components/Properties.jsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "./Spinner";
+
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch("/api/properties");
+
+        if (res.status === 200) {
+          const propertiesData = await res.json();
+          const { data } = propertiesData;
+          setProperties(data);
+        }
+      } catch (error) {
+        console.log("fetch properties error ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4'>
+        {properties.length === 0 ? (
+          <p>No Properties Found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Properties;
